f03e0f86b2385b56fa9782820db3f0673b7c1b18
uzakotim <timauzakov@gmail.com>
2024-03-12 10:18:15 +0500

    start_pm2.bat

--- start_pm2.bat
+++ start_pm2.bat
@@ -1,8 +1,16 @@
-echo off
+@echo off
 REM Change directory to your Node.js app directory
-cd /d "C:\"
+cd /d "C:\Users\user\git\node_car_controller\backend\src"
+
 REM Start PM2 and your Node.js app
-pm2 start app.js --name "my-node-app"
+pm2 start command_server.js --name "command-server"
+
+REM Change directory to your Node.js app directory
+cd /d "C:\Users\user\git\node_car_controller\conrtoller\src"
+
+REM Start PM2 and your Node.js app
+pm2 start conrtoller.js --name "controller"
+
 REM Wait for a few seconds to ensure PM2 has started
 timeout /t 5 /nobreak 62nul
 
