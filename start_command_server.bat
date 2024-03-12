3fede8967d6615bc620eec0cb92a4bf59b77db16
uzakotim <timauzakov@gmail.com>
2024-03-12 11:40:26 +0500

    greater timeout

--- start_command_server.bat
+++ start_command_server.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "C:\Users\user\git\node_car_controller\backend\src"
 pm2 start command_server.js --name "command-server"
-timeout /t 5 /nobreak >nul
+timeout /t 10 /nobreak >nul
 exit
--- start_controller.bat
+++ start_controller.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "C:\Users\user\git\node_car_controller\controller\src"
 pm2 start controller.js --name "controller-nodejs"
-timeout /t 5 /nobreak >nul
+timeout /t 10 /nobreak >nul
 exit
