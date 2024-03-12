b97872884ba087b03a68dc9d0be2886d8a1654b6
uzakotim <timauzakov@gmail.com>
2024-03-12 11:21:02 +0500

    sub as task

--- test/subscriber.py
+++ test/subscriber.py
@@ -1,11 +1,21 @@
 import zmq
-#create a ZeroMQ subscriber
+from threading import Thread
+from time import sleep
 
-context = zmq.Context()
-subscriber = context.socket(zmq.SUB)
-subscriber.connect("tcp://127.0.0.1:8081")
-subscriber.setsockopt_string(zmq.SUBSCRIBE, "")
 
-while True:
-    string = subscriber.recv_string()
-    print(string)
+
+def listener(delay):
+    #create a ZeroMQ subscriber
+    context = zmq.Context()
+    subscriber = context.socket(zmq.SUB)
+    subscriber.connect("tcp://127.0.0.1:8081")
+    subscriber.setsockopt_string(zmq.SUBSCRIBE, "")
+    while True:
+        string = subscriber.recv_string()
+        print(string)
+        sleep(delay)
+
+if __name__ == "__main__":
+    p1 = Thread(target=listener,args=([0.01]))
+    p1.start()
+    p1.join()
